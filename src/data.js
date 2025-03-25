1e33bd9ce22471a252738623eb62226e8c1e94b4
Taufiqsaefulloh20102237 <20102237@ittelkom-pwt.ac.id>
2025-03-26 07:28:34 +0800

    updatez

--- src/data.js
+++ src/data.js
@@ -2,21 +2,21 @@ const getData = () => {
     return [
         {
             id: 1,
-            name: 'Dimas Saputra',
-            tag: 'dimasmds',
-            imageUrl: 'assets/images/dimasmds.jpeg',
+            name: 'Taufiq Saefulloh',
+            tag: 'toopiq_',
+            imageUrl: 'assets/images/default.jpeg',
         },
         {
             id: 2,
-            name: 'Arif Faizin',
-            tag: 'arifaizin',
-            imageUrl: 'assets/images/arifaizin.jpeg',
+            name: 'Bambang Aja',
+            tag: 'Bbg_',
+            imageUrl: 'assets/images/default.jpeg',
         },
         {
             id: 3,
             name: 'Rahmat Fajri',
             tag: 'rfajri27',
-            imageUrl: 'assets/images/rfajri27.jpeg',
+            imageUrl: 'assets/images/default.jpeg',
         },
     ];
 }
